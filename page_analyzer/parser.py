a9112eded10dab4398b7d1234ce5941e1e9ac6dd
anik0000000 <idozmor@gmail.com>
2025-08-21 11:47:41 +0500

    RUFF fix

--- page_analyzer/parser.py
+++ page_analyzer/parser.py
@@ -5,13 +5,7 @@ from bs4 import BeautifulSoup
 def analyze_url(url):
     """Анализ URL и извлечение SEO-информации"""
     try:
-        headers = {
-            'User-Agent': (
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                'AppleWebKit/537.36'
-            )
-        }
-        response = requests.get(url, headers=headers, timeout=10)
+        response = requests.get(url, timeout=10)
         response.raise_for_status()
 
         soup = BeautifulSoup(response.text, "html.parser")
